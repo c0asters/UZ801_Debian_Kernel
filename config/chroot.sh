f3ab6c77911e97bb97e5697721c8a2c2e49d26ea
JackAltman <82563979+JackA1ltman@users.noreply.github.com>
2025-01-19 18:59:59 +0800

    Update chroot.sh

--- config/chroot.sh
+++ config/chroot.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+USERNAME="ubuntu"
+PASSWORD="123456"
 /debootstrap/debootstrap --second-stage
 echo "locales locales/default_environment_locale select en_US.UTF-8" | debconf-set-selections
 echo "locales locales/locales_to_be_generated multiselect en_US.UTF-8 UTF-8" | debconf-set-selections
@@ -7,5 +9,6 @@ dpkg-reconfigure --frontend noninteractive locales
 dpkg -i root/*.deb
 apt update
 apt install usbutils network-manager sudo fdisk vim nano openssh-server iputils-ping wget curl iproute2 locales kmod zip unzip u-boot-tools initramfs-tools net-tools htop -y
-adduser ubuntu
+echo -e "$PASSWORD\n$PASSWORD" | adduser --gecos "" $USERNAME
+usermod -aG sudo $USERNAME
 exit
